9c25b32f7f7a025dd2697fc2c9f3f44413cdef81
shankarsantoshgorle <shankarsantosh.gorle88@gmail.com>
2022-08-23 21:11:58 +0530

    added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ function App() {
       <h1>Amrutha Varshini</h1>
       <h1>Gorle Ramu</h1>
       <h1>Gorle Lakshmi</h1>
+      <h1>Gorle Padma</h1>
+      <h1>Gorle Siva Prasad</h1>
+      <h1>Gorle Bala Chander Rao</h1>
     </div>
   );
 }
